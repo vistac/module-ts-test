61f59a881149e5f00fb058a0114a1e28fe28e629
vistac <vistac.wu@gmail.com>
2024-06-06 15:21:36 +0800

    comp

--- src/comp.tsx
+++ src/comp.tsx
@@ -35,9 +35,30 @@ const Counter = () => (
 		<Text>Example</Text>
 	</>
 );
+import { DownloaderHelper, DownloaderHelperOptions } from 'node-downloader-helper';
+(async () => {
 
-const __filename = fileURLToPath(import.meta.url);
-console.log(cli.flags);
-const infoHash = dotTorrent.parse(fs.readFileSync('aa.torrent')).infoHash;
-console.log(infoHash);
-render(<Counter />);
+	const url = 'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
+	const __filename = fileURLToPath(import.meta.url);
+	console.log(cli.flags);
+	const infoHash = dotTorrent.parse(fs.readFileSync('aa.torrent')).infoHash;
+	console.log(infoHash);
+	const downloadOption: DownloaderHelperOptions = {
+		headers: {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0'
+		},
+		retry: true,
+		resumeOnIncompleteMaxRetry: 20,
+		fileName: 'aaa.mp4',
+		override: false
+	};
+	const downloader = new DownloaderHelper(url, './', downloadOption);
+
+	// downloader.updateOptions({}, 'http://www.google.com');
+	await downloader.start().catch(e => console.log('err', e));
+	downloader.updateOptions({}, 'http://abba.ddnsking.com/aa.jpg');
+	await downloader.start().catch(e => console.log('err', e));
+	console.log(downloader.getOptions());
+
+	render(<Counter />);
+})();
