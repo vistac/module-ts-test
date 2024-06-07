ae6b5376d717a257ae51570d2d9c25f5412bd4ec
vistac <vistac.wu@gmail.com>
2024-06-07 16:50:19 +0800

    help

--- src/queueRenamer.ts
+++ src/queueRenamer.ts
@@ -2,52 +2,59 @@
 import child_process from 'child_process';
 import dotTorrent from 'dot-torrent';
 import fs from "fs";
-// import getopts from 'getopts';
-import JSON5 from 'json5';
+import meow from 'meow';
 import moment from "moment-timezone";
 import os from 'os';
 import path from "path";
 import { fileURLToPath } from 'url';
 import { createLogger, format, transports } from "winston";
 import "winston-daily-rotate-file";
-// import meow from 'meow';
+import { getConfig, timeZone } from './utils.js';
 
-// const __filename = fileURLToPath(import.meta.url);
-const username = 'vistac';
-const scanDir = process.argv[2] || '//bthome//torrent//public//download//';
-const targetDir = process.argv[3] || '//bthome//torrent//public//queue//';
-const configFile = process.argv[2] || path.join(os.homedir(), 'etc', `sukebei-parser.json`);
-const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const __filename = fileURLToPath(import.meta.url);
 const appName = path.parse(__filename)['name'] || 'App';
-const platform = os.platform();
-// const cli = meow(
-// 	`
-// 		rename filename to hash
-// 		Usage
-// 			$ fooo
-// 	`
-// 	, {
-// 		importMeta: import.meta,
-// 		flags: {
-// 			help: {
-// 				shortFlag: 'h'
-// 			},
-// 			test: {
-// 				type: 'boolean',
-// 				shortFlag: 't'
-// 			}
-// 		}
-// 	});
-// const options = getopts(process.argv.slice(2), {});
-// const options = cli.flags;
 
-const getConfig = async () => {
-	if (await fs.existsSync(configFile)) {
-		return JSON5.parse(fs.readFileSync(configFile).toString());
-	} else {
-		return { includeKeywords: [], downloadTo: path.join(os.homedir(), 'torrents') };
+const cli = meow(
+	`
+Name
+	queueRenamer - rename file to {info hash}.torrent
+
+SYNOPSIS
+	queueRenamer [OPTIONS]
+	
+DESCRIPTION
+	--dryrun,-d 
+		dry run.
+	--config,-c 
+		input config file.
+	--help,-h 
+		show help.
+	--verbose,-v 
+		verbose.
+
+	`, {
+	importMeta: import.meta,
+	allowUnknownFlags: false,
+	flags: {
+		help: {
+			shortFlag: 'h'
+		},
+		config: {
+			shortFlag: 'c',
+			type: 'string',
+			default: path.join(os.homedir(), 'etc', `sukebei-parser.jsonc`),
+		},
+		dryrun: {
+			shortFlag: 'd',
+			type: 'boolean'
+		},
+		verbose: {
+			shortFlag: 'v',
+			type: 'boolean'
+		}
 	}
-};
+}
+);
 
 const defaultConfig = {
 	rssUrl: 'https://sukebei.nyaa.si/?page=rss',
@@ -90,32 +97,32 @@ const logger = createLogger({
 });
 
 const getUserUidGid = (username: string) => {
-	let ret = {
+	const ret = {
 		uid: -1,
 		gid: -1
 	};
 	try {
-		let uid = parseInt(child_process.execSync(`id -u ${username}`).toString().replace(/\n/g, ''));
-		let gid = parseInt(child_process.execSync(`id -g ${username}`).toString().replace(/\n/g, ''));
+		const uid = parseInt(child_process.execSync(`id -u ${username}`).toString().replace(/\n/g, ''));
+		const gid = parseInt(child_process.execSync(`id -g ${username}`).toString().replace(/\n/g, ''));
 		ret['uid'] = isNaN(uid) ? -1 : uid;
 		ret['gid'] = isNaN(gid) ? -1 : gid;
 	} catch (e) { }
 	return ret;
 };
 (async () => {
-	let config = { ...defaultConfig, ...(await getConfig()) };
-	let userInfo = getUserUidGid(config['username'] || '');
-	let fullScanDir = path.resolve(config['scanDir'] || '');
-	let fullTargetDir = path.resolve(config['targetDir'] || '');
+	const config = await getConfig(cli.flags['config'] || '', defaultConfig);
+	const userInfo = getUserUidGid(config['username'] || '');
+	const fullScanDir = path.resolve(config['scanDir'] || '');
+	const fullTargetDir = path.resolve(config['targetDir'] || '');
 	if ((fs.existsSync(fullScanDir) && fs.lstatSync(fullScanDir).isDirectory()) == false) return;
 	if ((fs.existsSync(fullTargetDir) && fs.lstatSync(fullTargetDir).isDirectory()) == false) return;
-	let files = fs.readdirSync(fullScanDir)
+	const files = fs.readdirSync(fullScanDir)
 		.map(x => path.join(fullScanDir, x))
 		.filter(x => fs.lstatSync(x).isFile());
-	for (let file of files) {
+	for (const file of files) {
 		try {
-			let infoHash = dotTorrent.parse(fs.readFileSync(file)).infoHash.toUpperCase() || '';
-			let output = path.join(fullTargetDir, `${infoHash}.torrent`);
+			const infoHash = dotTorrent.parse(fs.readFileSync(file)).infoHash.toUpperCase() || '';
+			const output = path.join(fullTargetDir, `${infoHash}.torrent`);
 			fs.chownSync(file, userInfo.uid, userInfo.gid);
 			console.log(`chown to ${userInfo.uid}.${userInfo.gid} for file ${file}`);
 			fs.chmodSync(file, 0o644);
