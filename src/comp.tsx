6e346f1a8e6c6d30a086fd8f26f7dbf1ec59d616
vistac <vistac.wu@gmail.com>
2024-06-07 11:18:58 +0800

    aa

--- src/comp.tsx
+++ src/comp.tsx
@@ -4,18 +4,26 @@ import { render, Text } from 'ink';
 import meow from 'meow';
 import { fileURLToPath } from 'url';
 import { doNothing } from './utils.js';
+import type { ExtendedAnyFlags } from 'meow-helper';
+const testFlags: ExtendedAnyFlags = {
+	help: { alias: 'h', type: 'boolean', desc: 'help' },
+	name: { alias: 'n', type: 'string', desc: 'Name' },
+	cwd: { alias: 'c', type: 'string', desc: 'Working path.' },
+	context: { type: 'string', desc: 'Context.' },
+};
 const cli = meow(
 	`
 		Usage
-			$ fooo
-	`
-	, {
+			$ foo
+	`,
+	{
 		importMeta: import.meta,
+		allowUnknownFlags: false,
 		flags: {
 			help: {
-				shortFlag: 'h'
+				shortFlag: 'h',
 			},
-			ranbow: {
+			rainbow: {
 				type: 'boolean',
 				shortFlag: 'r',
 			},
@@ -23,36 +31,40 @@ const cli = meow(
 				type: 'boolean',
 				shortFlag: 'd',
 			},
-			showleft: {
+			showLeft: {
 				type: 'boolean',
 				shortFlag: 'l',
-			}
-		}
-
-	});
+			},
+		},
+	}
+);
 
 const Counter = () => (
 	<>
 		<Text>Example</Text>
 	</>
 );
-import { DownloaderHelper, DownloaderHelperOptions } from 'node-downloader-helper';
+import {
+	DownloaderHelper,
+	DownloaderHelperOptions,
+} from 'node-downloader-helper';
 import Parser from 'rss-parser';
 (async () => {
-
-	const url = 'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
+	const url =
+		'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
 	const __filename = fileURLToPath(import.meta.url);
 	console.log(cli.flags);
 	// const infoHash = dotTorrent.parse(fs.readFileSync('aa.torrent')).infoHash;
 	// console.log(infoHash);
 	const downloadOption: DownloaderHelperOptions = {
 		headers: {
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0'
+			'User-Agent':
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0',
 		},
 		retry: true,
 		resumeOnIncompleteMaxRetry: 20,
 		fileName: 'aaa.mp4',
-		override: false
+		override: false,
 	};
 	const downloader = new DownloaderHelper(url, './', downloadOption);
 
