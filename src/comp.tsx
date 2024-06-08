6096c650eeaa9df22ef7ba5b196901dc0d9a7844
vistac <vistac.wu@gmail.com>
2024-06-08 20:11:35 +0800

    hh

--- src/comp.tsx
+++ src/comp.tsx
@@ -10,6 +10,7 @@ const aa = async () => { console.log(`12`); return 'aa'; };
 const bb = await aa();
 console.log(bb);
 nullishCoalescing(0, "");
+console.log(`%cMy name is %cAdam`, 'color:green', 'color: blue');
 // (async () => {
 // 	const url =
 // 		'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
