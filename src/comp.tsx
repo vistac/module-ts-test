a95af71b276f52621d8321cf437e81d5e975925c
vistac <vistac.wu@gmail.com>
2024-06-08 16:49:10 +0800

    test top level await

--- src/comp.tsx
+++ src/comp.tsx
@@ -6,10 +6,13 @@ const Counter = () => (
 		<Text>Example</Text>
 	</>
 );
-(async () => {
-	const url =
-		'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
-	const __filename = fileURLToPath(import.meta.url);
+const aa = async () => { console.log(`12`); return 'aa'; };
+const bb = await aa();
+console.log(bb);
+// (async () => {
+// 	const url =
+// 		'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
+// 	const __filename = fileURLToPath(import.meta.url);
 
-	render(<Counter />);
-})();
+// 	render(<Counter />);
+// })();
