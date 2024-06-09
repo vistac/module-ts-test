6bdce8f062d24b7085d7d838d9301984d266b822
vistac <vistac.wu@gmail.com>
2024-06-09 15:02:38 +0800

    test

--- src/comp.tsx
+++ src/comp.tsx
@@ -1,20 +1,53 @@
 import { Text } from 'ink';
-import { nullishCoalescing } from './utils.js';
+import { nullishCoalescing, randomRange, timeZone } from './utils.js';
+import { createLogger, format, transports } from 'winston';
+import moment from 'moment';
+import "winston-daily-rotate-file";
+import { randomInt } from 'crypto';
 
 const Counter = () => (
 	<>
 		<Text>Example</Text>
 	</>
 );
-const aa = async () => { console.log(`12`); return 'aa'; };
-const bb = await aa();
-console.log(bb);
 nullishCoalescing(0, "");
-console.log(`%cMy name is %cAdam`, 'color:green', 'color: blue');
-// (async () => {
-// 	const url =
-// 		'https://file-examples.com/wp-content/storage/2017/04/file_example_MP4_480_1_5MG.mp4';
-// 	const __filename = fileURLToPath(import.meta.url);
 
-// 	render(<Counter />);
-// })();
+const consoleTransport = new transports.Console({});
+const fileTransport = new transports.DailyRotateFile({
+	level: 'info',
+	filename: '//tmp/test-%DATE%.log',
+	auditFile: '//tmp//test.log.json',
+	symlinkName: 'test.log',
+	createSymlink: true,
+	zippedArchive: true,
+	maxSize: '10m',
+	maxFiles: '7d'
+});
+const logger = createLogger({
+	transports: [
+		fileTransport,
+		consoleTransport,
+	],
+	format: format.combine(
+		format.label({ label: 'Test' }),
+		format.colorize(),
+		format.prettyPrint(),
+		format.printf((info) => {
+			if (typeof (info.message) === 'string') {
+				return `[${moment().tz(timeZone).format()}] [${info.label}] ${info.level} ${info.message}`;
+			} else {
+				return `[${moment().tz(timeZone).format()}] [${info.label}] ${info.level}\n${JSON.stringify(info.message, null, 2)}`;
+			}
+		})
+	)
+});
+const myFormat = format.combine(format.label({ label: 'abc' }));
+console.log(myFormat.options);
+logger.info('123');
+
+(async () => {
+	for (const i of Array(9).keys()) {
+		const aa = randomInt(9999999).toString().padStart(9, '0');
+		// logger.info(`${i} => ${aa}`);
+	}
+})();
