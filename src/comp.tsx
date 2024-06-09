3136fa477c76b0240e7ec903a4236f3a75b78b61
vistac <vistac.wu@gmail.com>
2024-06-09 21:14:55 +0800

    test

--- src/comp.tsx
+++ src/comp.tsx
@@ -1,8 +1,5 @@
 import { Text } from 'ink';
-import { nullishCoalescing, randomRange, timeZone } from './utils.js';
-import { createLogger, format, transports } from 'winston';
-import moment from 'moment';
-import "winston-daily-rotate-file";
+import { getLogFormat, nullishCoalescing, randomRange, timeZone, logger } from './utils.js';
 import { randomInt } from 'crypto';
 
 const Counter = () => (
@@ -12,42 +9,15 @@ const Counter = () => (
 );
 nullishCoalescing(0, "");
 
-const consoleTransport = new transports.Console({});
-const fileTransport = new transports.DailyRotateFile({
-	level: 'info',
-	filename: '//tmp/test-%DATE%.log',
-	auditFile: '//tmp//test.log.json',
-	symlinkName: 'test.log',
-	createSymlink: true,
-	zippedArchive: true,
-	maxSize: '10m',
-	maxFiles: '7d'
-});
-const logger = createLogger({
-	transports: [
-		fileTransport,
-		consoleTransport,
-	],
-	format: format.combine(
-		format.label({ label: 'Test' }),
-		format.colorize(),
-		format.prettyPrint(),
-		format.printf((info) => {
-			if (typeof (info.message) === 'string') {
-				return `[${moment().tz(timeZone).format()}] [${info.label}] ${info.level} ${info.message}`;
-			} else {
-				return `[${moment().tz(timeZone).format()}] [${info.label}] ${info.level}\n${JSON.stringify(info.message, null, 2)}`;
-			}
-		})
-	)
-});
-const myFormat = format.combine(format.label({ label: 'abc' }));
-console.log(myFormat.options);
-logger.info('123');
+logger.format = getLogFormat('comp');
+console.log(logger);
+
 
 (async () => {
+	const aa = randomInt(9999999).toString().padStart(9, '0');
+
+	logger.info(aa);
 	for (const i of Array(9).keys()) {
-		const aa = randomInt(9999999).toString().padStart(9, '0');
 		// logger.info(`${i} => ${aa}`);
 	}
 })();
