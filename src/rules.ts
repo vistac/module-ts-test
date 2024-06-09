2df511a639b1b6b5e20023ff65c86e9e834eba6f
vistac <vistac.wu@gmail.com>
2024-06-10 01:49:04 +0800

    rules

---
+++ src/rules.ts
@@ -0,0 +1,25 @@
+export class RenameRules {
+	static renameByRule = (srcString: string, policy: string[]) => {
+		let result = srcString;
+		if (eval(`typeof ${policy[2]} === 'function'`)) {
+			result = eval(`${policy[2]}('${srcString}', '${policy[0]}', '${policy[1]}')`);
+		}
+		return result;
+	};
+
+	static ruleJavch = (srcString: string, pattern: string, replacement: string): string => {
+		return srcString.replace(new RegExp(pattern, 'i'), (...args) => `${args[1].toUpperCase()}-${args[2]}-C.mp4`);
+	};
+
+	static ruleUpper = (srcString: string, pattern: string, replacement: string): string => {
+		return srcString.replace(new RegExp(pattern, 'i'), (...args) => `${args[1].toUpperCase()}-${args[2]}.mp4`);
+	};
+
+	static ruleRemove = (srcString: string, pattern: string, replacement: string): string => {
+		return srcString.replace(new RegExp(pattern, 'i'), (...args) => `${args[1].toUpperCase()}.mp4`);
+	};
+
+	static ruleNormal = (srcString: string, pattern: string, replacement: string): string => {
+		return srcString.replace(new RegExp(pattern, 'i'), replacement);
+	};
+}
